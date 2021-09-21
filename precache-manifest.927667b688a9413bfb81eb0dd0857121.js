8437c2de08a97aa0adbe0115a87b1e58571f52e5
Marcel Winheim <github2021@winhe.im>
2021-09-21 17:22:40 +0200

    Updates

---
+++ precache-manifest.927667b688a9413bfb81eb0dd0857121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdfdb4b17e430d60d91ef7dc6283561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb8baaf4e00e9a43aac",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "988925c59ec762cca015",
+    "url": "/static/js/2.ce22408f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ce22408f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb8baaf4e00e9a43aac",
+    "url": "/static/js/main.819dfccc.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  }
+]);
